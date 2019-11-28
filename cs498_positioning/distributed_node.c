0ed9b20b88a52969e30ae0f25319af11a157843e
keruiz2 <keruiz2@illinois.edu>
2019-11-28 15:54:29 -0600

    delete useless c files

--- cs498_positioning/distributed_node.c
+++
@@ -1,3 +0,0 @@
-#include "distributed_node.h"
-
-
--- cs498_positioning/port.py
+++ cs498_positioning/port.py
@@ -1,5 +1,6 @@
 import serial
 import numpy as np
+import telnetlib
 
 ref_nodes = {}
 pos = np.array([0])
@@ -7,11 +8,17 @@ pos = np.array([0])
 def grab_and_cal(info):
     global ref_nodes
     global pos
+    # print(info)
     dist = {}
     rough_split = info.split('[')
+    if len(rough_split) <= 2:
+        return -1
     dis_list = rough_split[1].split(']')[0].split(',')
+    if len(dis_list) < 4:
+        return -1
     id_list = rough_split[2].split(']')[0].split(',')
-    
+    if len(id_list) < 4:
+        return -1
     if (len(dis_list) != len(id_list)) | (len(dis_list) < 4):
         return -1
     
@@ -19,7 +26,7 @@ def grab_and_cal(info):
     for i in range(0, len(dis_list)):
         id_list[i] = id_list[i].strip('"')
         if id_list[i] in ref_nodes:
-            dist[id_list[i]] = (int)(dis_list[i].strip('"'))
+            dist[id_list[i]] = (float)(dis_list[i].strip('"'))
             if base == "":
                 base = id_list[i]
     
@@ -40,27 +47,43 @@ def grab_and_cal(info):
     
     AT = np.transpose(A)
     B = np.dot(AT, B)
+    # print(A)
     rev = np.linalg.inv(np.dot(AT, A))
     pos = np.dot(rev, B)
-    print(pos)
+    posT = np.transpose(pos)
+    print(posT)
     return 1
-
-
-# def read_port():
 
 
 def init():
     global ref_nodes
     
-    ref_nodes["1818"] = np.array([3000, 0, 0])
-    ref_nodes["5632"] = np.array([0, 2500, 0])
-    ref_nodes["3884"] = np.array([0, 1500, 0])
-    ref_nodes["1665"] = np.array([0, 0, 800])
+    ref_nodes["1818"] = np.array([0, 0, 0])
+    ref_nodes["5632"] = np.array([3.2, 0, 0])
+    ref_nodes["3884"] = np.array([0, 4.8, 0])
+    ref_nodes["1665"] = np.array([2.6, 4.8, 2.15])
+
+def command(con, flag):
+    data = con.read_until(flag.encode())
+    print(data.decode(errors='ignore'))
+    return data
 
 
 
 init()
-print(ref_nodes)
-print(grab_and_cal("{{rng:[\"3256\",\"2222\",\"3808\",\"1189\"], \"uid\": [\"1818\",\"5632\",\"3884\",\"1665\"]}}"))
+# print(ref_nodes)
+# print(grab_and_cal("{{rng:[\"3256\",\"2222\",\"3808\",\"1189\"], \"uid\": [\"1818\",\"5632\",\"3884\",\"1665\"]}}"))
+# serial_name = ''
+# ser = serial.Serial(serial_name)
+# while True:
+#     line = ser.readline()
+#     grab_and_cal(line)
+tn = telnetlib.Telnet(host="127.0.0.1", port=19021)
+# grab_and_cal("{\"utime\": 1322787487,\"nrng\": {\"seq\": 193,\"mask\": 15,\"rng\": [\"3.513\",\"3.909\",\"2.267\",\"2.680\"],\"uid\": [\"1818\",\"5632\",\"3884\",\"1665\"]}}")
+while True:
+    raw_data = tn.read_until('\n'.encode())
+    data = raw_data.decode(errors='ignore')
+    # print(data)
+    grab_and_cal(data)
 
-    
+tn.close()  
