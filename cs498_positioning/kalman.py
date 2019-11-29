68de8dadf373c933bbfecf2b0763f425ed0d0f19
keruiz2 <keruiz2@illinois.edu>
2019-11-29 17:58:48 -0600

    program may have bug

--- cs498_positioning/kalman.py
+++ cs498_positioning/kalman.py
@@ -1,21 +1,21 @@
 from pykalman import KalmanFilter
 import numpy as np
 
-kf = KalmanFilter(transition_matrices=np.array([[1, 0, 0, 1, 0, 0],
-                                                [0, 1, 0, 0, 1, 0],
-                                                [0, 0, 1, 0, 0, 1],
-                                                [0, 0, 0, 1, 0, 0],
-                                                [0, 0, 0, 0, 1, 0],
-                                                [0, 0, 0, 0, 0, 1]]),
-                  observation_matrices=np.array([[1, 0, 0, 0, 0, 0],
-                                                 [0, 1, 0, 0, 0, 0],
-                                                 [0, 0, 1, 0, 0, 0]]),
-                  transition_covariance=0.03 * np.eye(6))  # TODO: change this constant
+kf = KalmanFilter(transition_matrices=np.array([[1.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+                                                [0.0, 1.0, 0.0, 0.0, 1.0, 0.0],
+                                                [0.0, 0.0, 1.0, 0.0, 0.0, 1.0],
+                                                [0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+                                                [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                                                [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]),
+                  observation_matrices=np.array([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                                 [0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                                                 [0.0, 0.0, 1.0, 0.0, 0.0, 0.0]]),
+                  transition_covariance=0.003 * np.eye(6, dtype=float))  # TODO: change this constant
 
 t = 0
 
 means = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-covariances = np.eye(6)
+covariances = np.eye(6, dtype=float)
 
 
 def kalman_filter(measurement):
@@ -23,6 +23,6 @@ def kalman_filter(measurement):
 
     new_filtered_means, new_filtered_covariances = (kf.filter_update(means, covariances, measurement))
     means, covariances = new_filtered_means, new_filtered_covariances
-    t = t + 1
-
-    return means
+    t = t + 1.0
+    # print(means[:3]);
+    return means[:3]
--- cs498_positioning/port.py
+++ cs498_positioning/port.py
@@ -4,15 +4,20 @@ import numpy as np
 import mpl_toolkits.mplot3d.axes3d as p3
 import telnetlib
 import threading
+import kalman
 import time
 
 ref_nodes = {}
-pos = np.array([[3],[0],[0]])
+pos = np.array([[0.0],[0.0],[0.0]])
+k_pos = np.array([[0.0],[0.0],[0.0]])
+acc_pos = np.array([[0.0],[0.0],[0.0]])
+count = 0
 
 def grab_and_cal(info):
     global ref_nodes
-    global pos
+    global pos, k_pos, acc_pos
     global ax
+    global count
 
     dist = {}
     rough_split = info.split('[')
@@ -38,7 +43,7 @@ def grab_and_cal(info):
     if len(dist) < 4:
         return -1
 
-    print(dist)
+    # print(dist)
     A = np.array([0,0,0])
     B = np.array([0])
     
@@ -56,7 +61,13 @@ def grab_and_cal(info):
     rev = np.linalg.inv(np.dot(AT, A))
     pos = np.dot(rev, B)
     posT = np.transpose(pos)
-    # print(posT)
+    pre_k = np.array([pos[0][0], pos[1][0], pos[2][0]])
+    kal_man = kalman.kalman_filter(pre_k)
+    k_pos[0][0] = kal_man[0]
+    k_pos[1][0] = kal_man[1]
+    k_pos[2][0] = kal_man[2]
+    acc_pos += pos
+    print(k_pos[0][0])
 
     return 1
 
@@ -64,7 +75,7 @@ def grab_and_cal(info):
 def init():
     global ref_nodes
     
-    ref_nodes["1818"] = np.array([0, 0, 0])
+    ref_nodes["1818"] = np.array([0.1, 1.0, 1.37])
     ref_nodes["5632"] = np.array([3.2, 0, 0])
     ref_nodes["3884"] = np.array([0, 4.8, 0])
     ref_nodes["1665"] = np.array([2.6, 4.8, 2.15])
@@ -77,6 +88,9 @@ class DataThread (threading.Thread):
             raw_data = tn.read_until('\n'.encode())
             data = raw_data.decode(errors='ignore')
             grab_and_cal(data)
+            f = open("data.txt","a")
+            f.write("x: " + str(pos[0][0]) + ", y: " + str(pos[1][0]) + ", z: " + str(pos[2][0]) + '\n')
+            f.close()
         tn.close()
 
 class PlotThread (threading.Thread):
@@ -87,7 +101,11 @@ class PlotThread (threading.Thread):
             plt.cla()
             
             ax.scatter(ref_xs, ref_ys, ref_zs, c='b', marker='*')
-            ax.scatter([pos[0]], [pos[1]], [pos[2]], c='r', marker='*')
+            # ax.scatter([pos[0]], [pos[1]], [pos[2]], c='r', marker='*')
+            ax.scatter([acc_pos[0]/count, acc_pos[1]/count, acc_pos[2]/count], c='r', marker='*')
+            acc_pos = np.array([[0.0],[0.0],[0.0]])
+            count = 0
+            # ax.scatter([k_pos[0]], [k_pos[1]], [k_pos[2]], c='g', marker='*')
             
             ax.set_xlim3d(-1, 4)
             ax.set_ylim3d(-1, 6)
